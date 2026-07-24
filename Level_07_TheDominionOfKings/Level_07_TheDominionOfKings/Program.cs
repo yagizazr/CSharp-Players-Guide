d12756331c771d2d6224d971346d3b4178d5b71b
yagizazr <mustafayagizazir@gmail.com>
2026-07-24 23:06:49 +0300

    Level_07_TheDominionOfKings

---
+++ Level_07_TheDominionOfKings/Level_07_TheDominionOfKings/Program.cs
@@ -0,0 +1,15 @@
+﻿    
+
+    Console.Write("Please enter the estate amount: ");
+    int estateAmount = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Please enter the duchy amount: ");
+    int duchyAmount = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Please enter the province amount: ");
+    int provinceAmount = Convert.ToInt32(Console.ReadLine());
+
+    int totalPoints = estateAmount + duchyAmount * 3 + provinceAmount * 6;
+    Console.WriteLine("Total points: " + totalPoints);
+
+    Console.ReadKey();
