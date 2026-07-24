067b6b1ed1bd5ea7fc6d8ebc840e0c5c569dd0b6
yagizazr <mustafayagizazir@gmail.com>
2026-07-24 22:12:28 +0300

    Level_07_TheTriangleFarmer

---
+++ Level_07_TheTriangleFarmer/Level_07_TheTriangleFarmer/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+Console.WriteLine("Please enter the base of the triangle: ");
+float baseLength = Convert.ToSingle(Console.ReadLine());
+
+Console.WriteLine("Please enter the height of the triangle: ");
+float height = Convert.ToSingle(Console.ReadLine());
+
+float area = (baseLength * height) / 2;
+Console.WriteLine("The area of the triangle is: " + area);
+
+
+Console.ReadKey();
