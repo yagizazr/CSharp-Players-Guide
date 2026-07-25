0b4c204c69c6d8b747b6f78697356139006b66b3
yagizazr <mustafayagizazir@gmail.com>
2026-07-25 23:56:55 +0300

    Level_08_TheDefenseOfConsolas

--- Level_08_TheDefenseOfConsolas/Level_08_TheDefenseOfConsolas/Program.cs
+++ Level_08_TheDefenseOfConsolas/Level_08_TheDefenseOfConsolas/Program.cs
@@ -1,14 +1,18 @@
 ﻿
-
+// Window title
 Console.Title = "The Defense of Consolas";
 
-Console.BackgroundColor = ConsoleColor.DarkBlue;
-Console.ForegroundColor = ConsoleColor.Magenta;
-
+// Asking the user for the target row and column
 Console.Write("Target Row: ");
 int row = Convert.ToInt32(Console.ReadLine());
 Console.Write("Target Column: ");
 int column = Convert.ToInt32(Console.ReadLine());
+
+// Display colors
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.ForegroundColor = ConsoleColor.Magenta;
+
+// Deploying the squad
 
 Console.WriteLine("Deploy to:");
 
@@ -17,13 +21,7 @@ Console.WriteLine($"({row - 1}, {column})");
 Console.WriteLine($"({row}, {column + 1})");
 Console.WriteLine($"({row + 1}, {column})");
 
-Task.Run(() => Console.Beep(37, 3000));
-
-// Kartın uyanması için yarım saniye bekleyin
-System.Threading.Thread.Sleep(500);
-
-// Artık 1 saniyelik veya yarım saniyelik kısa sesleriniz ANINDA çalacaktır
-
+// Playing a sound to indicate deployment
 Console.Beep(523, 100); 
 Console.Beep(659, 100); 
 Console.Beep(784, 250);
