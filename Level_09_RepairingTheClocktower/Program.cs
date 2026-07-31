16e51fed5c127cfef72598613cc0253a8e1c3803
yagizazr <mustafayagizazir@gmail.com>
2026-07-31 16:16:28 +0300

    Level_09_RepairingTheClocktower

---
+++ Level_09_RepairingTheClocktower/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+Console.Write("Please enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Tick.");
+}
+else
+{
+    Console.WriteLine("Tock.");
+}
+
+Console.ReadKey();
