41c53ad0f55bac82dbd7cd3e9f19945e777f2135
yagizazr <mustafayagizazir@gmail.com>
2026-08-03 15:45:01 +0300

    Level_11_TheMagicCannon

---
+++ Level_11_TheMagicCannon/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine($"{i}: Imaginary technique: Hollow Purple");
+    }
+
+    else if (i % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{i}: Cursed Technique Reversal: Red");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"{i}: Cursed Technique: Blue");
+
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine($"{i}: Normal");
+    }
+}
+
+Console.ReadKey();
