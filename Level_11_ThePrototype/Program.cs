497dd995dbeff9dc9cd64b8a75394d9258119489
yagizazr <mustafayagizazir@gmail.com>
2026-08-03 15:19:52 +0300

    Level_11_ThePrototype

---
+++ Level_11_ThePrototype/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+int correctNumber;
+
+do
+{
+    Console.Write("Player 1, please enter a number between 0 and 100: ");
+    correctNumber = Convert.ToInt32(Console.ReadLine());
+} 
+while (correctNumber < 0 || correctNumber > 100);
+
+Console.Clear();
+
+Console.WriteLine("Player 2, guess the number.");
+
+while(true)
+{
+    
+    Console.Write("What is your next guess? ");
+    int guess = Convert.ToInt32(Console.ReadLine());
+
+    if (guess < correctNumber) Console.WriteLine($"{guess} is too low.");
+    else if (guess > correctNumber) Console.WriteLine($"{guess} is too high.");
+    else break;
+
+}
+
+Console.WriteLine("You guessed the number!");
