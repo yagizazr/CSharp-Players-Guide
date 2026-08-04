d1fe23993f16589aa68f86bbebecca65c807dc93
yagizazr <mustafayagizazir@gmail.com>
2026-08-04 13:42:15 +0300

    Level_12_TheReplicatorOfDTo

---
+++ Level_12_TheReplicatorOfDTo/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+int[] original = new int[5];
+Console.WriteLine("Please enter five numbers.");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Enter number {i + 1}: ");
+    original[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int[] copy = new int[5];
+
+for (int i = 0 ; i < 5; i++)
+{
+    copy[i] = original[i];
+}
+
+for (int i = 0 ; i < 5; i++)
+{
+    Console.WriteLine($"{original[i]} to {copy[i]}");
+}
+
