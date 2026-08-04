b2f045c4df77bb7572165e7945351080ab9478f6
yagizazr <mustafayagizazir@gmail.com>
2026-08-04 14:15:25 +0300

    Level_12_TheLawsOfFreach

---
+++ Level_12_TheLawsOfFreach/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -809, 45, 90 };
+int smallest = int.MaxValue;
+int total = 0;
+float average = 0;
+
+foreach (int number in array)
+{
+    if (number < smallest)
+        smallest = number;
+
+    total += number;
+}
+
+average = (float)total / array.Length;
+
+Console.WriteLine(smallest);
+Console.WriteLine(average);
