292f2e6d3e96c044fe04d237723577e7c4258753
yagizazr <mustafayagizazir@gmail.com>
2026-08-06 16:58:00 +0300

    Level_13_TakingANumber

---
+++ Level_13_TakingANumber/Program.cs
@@ -0,0 +1,33 @@
+﻿int number = AskForNumberInRange("Player 1, please enter a number between 0 and 100:", 0, 100);
+
+Console.Clear();
+
+Console.WriteLine("Player 2, guess the number.");
+
+while (true)
+{
+    int guess = AskForNumber("What is your next guess?");
+    if (guess > number) Console.WriteLine($"{guess} is too high");
+    else if (guess < number) Console.WriteLine($"{guess} is too low");
+    else break;
+}
+
+Console.WriteLine("You guessed the number!");
+
+int AskForNumber(string text)
+{
+    Console.Write(text + " ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    while (true)
+    {
+        int number = AskForNumber(text);
+        if (number >= min && number <= max)
+        return number;
+    }
+}
+
