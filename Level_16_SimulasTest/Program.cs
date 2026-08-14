30b7a3b85795a644fab516751c26775761bdd0f4
yagizazr <mustafayagizazir@gmail.com>
2026-08-14 16:55:39 +0300

    Level_16_SimulasTest

---
+++ Level_16_SimulasTest/Program.cs
@@ -0,0 +1,29 @@
+﻿States current = States.locked;
+
+while (true)
+{
+    Console.Write($"The chest is {current}. What do you want to do ");
+    string text = Console.ReadLine();
+
+    if (current == States.locked && text == "unlock")
+    {
+        current = States.closed;
+    }
+
+    else if (current == States.closed && text == "lock")
+    {
+        current = States.locked;
+    }
+
+    else if (current == States.closed && text == "open")
+    {
+        current = States.open;
+    }
+
+    else if (current == States.open && text == "close")
+    {
+        current = States.closed;
+    }
+
+}
+enum States { locked, closed, open }
